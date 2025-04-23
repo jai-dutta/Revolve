de7103597d5d0b4cda4fd7dabc5d0bc1deadbbef
jai <jaitomdutta@gmail.com>
2025-04-23 22:41:50 +0800

    Backend project setup

---
+++ backend/src/main/java/com/jaidutta/revolve/RevolveApplication.java
@@ -0,0 +1,13 @@
+package com.jaidutta.revolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevolveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevolveApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jaidutta/revolve/RevolveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaidutta.revolve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevolveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
