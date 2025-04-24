6f0b8b6925981586d43b4687f735c9ae8c7f3116
jai-dutta <161834694+jai-dutta@users.noreply.github.com>
2025-04-24 22:30:58 +0800

    Create GlobalExceptionHandler.java
    Currently handles MethodArgumentNotValid exception from invalid DTO data provided

---
+++ backend/src/main/java/com/jaidutta/revolve/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.jaidutta.revolve.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.View;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private final View error;
+
+    public GlobalExceptionHandler(View error) {
+        this.error = error;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+
+        for (ObjectError objectError : exception.getBindingResult().getAllErrors()) {
+            errors.put( ((FieldError) error).getField(), objectError.getDefaultMessage());
+        }
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
