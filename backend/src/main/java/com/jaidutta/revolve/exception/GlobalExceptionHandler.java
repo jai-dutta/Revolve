e0ca59776a5b914518b9331939b87a327f5461eb
jai-dutta <161834694+jai-dutta@users.noreply.github.com>
2025-04-24 22:49:18 +0800

    Fix GlobalExceptionHandler
    Had an issue with using View, removed that.

--- backend/src/main/java/com/jaidutta/revolve/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/jaidutta/revolve/exception/GlobalExceptionHandler.java
@@ -2,13 +2,10 @@ package com.jaidutta.revolve.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
+
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.View;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,20 +13,16 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    private final View error;
-
-    public GlobalExceptionHandler(View error) {
-        this.error = error;
-    }
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException exception) {
         Map<String, String> errors = new HashMap<>();
 
-        for (ObjectError objectError : exception.getBindingResult().getAllErrors()) {
-            errors.put( ((FieldError) error).getField(), objectError.getDefaultMessage());
-        }
+        exception.getBindingResult().getFieldErrors().forEach(fieldError -> {
+            String fieldName = fieldError.getField();
+            String errorMessage = fieldError.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
