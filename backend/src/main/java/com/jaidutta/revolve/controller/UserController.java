3b8c9678f2bbecda4aa86f821b0f17c4415c11f2
jai-dutta <161834694+jai-dutta@users.noreply.github.com>
2025-04-24 21:07:35 +0800

    Implement a test endpoint to verify correctness of user registration/login
    Implement a basic protected test endpoint (`GET /api/users/me`) to verify the end-to-end authentication and token validation flow.

--- backend/src/main/java/com/jaidutta/revolve/controller/UserController.java
+++ backend/src/main/java/com/jaidutta/revolve/controller/UserController.java
@@ -1,4 +1,27 @@
 package com.jaidutta.revolve.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/users")
 public class UserController {
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getCurrentUser() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated() && authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            String username = userDetails.getUsername();
+            return ResponseEntity.ok("Hello, authenticated user: " + username);
+        } else {
+            return ResponseEntity.status(401).body("User not authenticated");
+        }
+    }
 }
