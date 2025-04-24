9988815825af209d960c19e85a4d29e548599739
jai-dutta <161834694+jai-dutta@users.noreply.github.com>
2025-04-24 15:42:23 +0800

    Add AuthController and AuthService and implement Registration
    Introduce AuthController and AuthService. Implemented Registration checking with password encoding and a RegisterRequestDto to move information.

---
+++ backend/src/main/java/com/jaidutta/revolve/controller/dto/RegisterRequestDto.java
@@ -0,0 +1,20 @@
+package com.jaidutta.revolve.controller.dto;
+
+
+public class RegisterRequestDto {
+
+    private final String username;
+    private final String password;
+
+    public RegisterRequestDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+}
