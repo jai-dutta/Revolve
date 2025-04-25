7c7ff6bcdc0ad66b244e347051cf3669093b17e0
jai <jaitomdutta@gmail.com>
2025-04-25 23:24:05 +0800

    Rework UserController
    Rely on security filtering instead of hard authentication checks

--- backend/src/main/java/com/jaidutta/revolve/controller/UserController.java
+++ backend/src/main/java/com/jaidutta/revolve/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.jaidutta.revolve.controller;
 
+import com.jaidutta.revolve.controller.dto.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -8,20 +10,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
 @RequestMapping("/api/users")
 public class UserController {
 
     @GetMapping("/me")
     public ResponseEntity<?> getCurrentUser() {
-
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated() && authentication.getPrincipal() instanceof UserDetails) {
-            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        if (authentication.getPrincipal() instanceof UserDetails userDetails) {
             String username = userDetails.getUsername();
-            return ResponseEntity.ok("Hello, authenticated user: " + username);
+            return ResponseEntity.ok(ApiResponse.success("Hello " + username));
         } else {
-            return ResponseEntity.status(401).body("User not authenticated");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(ApiResponse.error("server.error", "Unexpected principal type found"));
         }
     }
 }
