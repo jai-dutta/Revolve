5ca30e43e42e0874435e1ddfbd602a23d49759c0
jai-dutta <161834694+jai-dutta@users.noreply.github.com>
2025-04-30 16:49:23 +0800

    Create RecurringActivityRepository

---
+++ backend/src/main/java/com/jaidutta/revolve/repository/RecurringActivityRepository.java
@@ -0,0 +1,16 @@
+package com.jaidutta.revolve.repository;
+
+import com.jaidutta.revolve.entity.RecurringActivity;
+import com.jaidutta.revolve.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RecurringActivityRepository extends JpaRepository<RecurringActivity, Long> {
+    List<RecurringActivity> findAllByUser(User user);
+    
+    Optional<RecurringActivity> findByIdAndUser(Long id, User user);
+}
