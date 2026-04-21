7c923c2ce403a65909584100762483e4f0d7d37e
jai <jaitomdutta@gmail.com>
2026-04-21 20:01:16 +0800

    Create ActivityInstanceNotFoundException.java

---
+++ backend/src/main/java/com/jaidutta/revolve/exception/ActivityInstanceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.jaidutta.revolve.exception;
+
+public class ActivityInstanceNotFoundException extends RuntimeException {
+    public ActivityInstanceNotFoundException(String message) {
+        super(message);
+    }
+}
